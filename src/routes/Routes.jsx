621d0c0bd0ae504086e8d4e79de05665ef4eacd1
Mohammed-Saiful-Islam <mosai21498@gmail.com>
2024-03-28 16:28:51 +0600

    some page link added into nav bar links

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,5 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayouts from "../layouts/MainLayouts";
+import Home from "../pages/Home";
+import ListedBooks from "../pages/ListedBooks";
+import PagesToRead from "../pages/PagesToRead";
 
 export const router = createBrowserRouter(
     [
@@ -8,7 +11,16 @@ export const router = createBrowserRouter(
             element: <MainLayouts />,
             children: [
                 {
-
+                    path: '/',
+                    element: <Home />
+                },
+                {
+                    path: '/listedbooks',
+                    element: <ListedBooks />
+                },
+                {
+                    path: 'pagestoread',
+                    element: <PagesToRead />
                 }
             ]
         }
