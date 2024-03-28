a91f37c37bd6ffa39efa6fb43063b9e97c076eda
Mohammed-Saiful-Islam <mosai21498@gmail.com>
2024-03-28 19:56:43 +0600

    rating start added from reat icons

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,4 +1,4 @@
-
+import { FiStar } from "react-icons/fi";
 const Books = ({ book }) => {
     /* 
     {bookName,author,image,review,totalPages,rating,category,tags,publisher,yearOfPublishing}
@@ -11,11 +11,12 @@ const Books = ({ book }) => {
                 <div className="p-6 space-y-2">
                     <p>{tags}</p>
                     <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">{bookName}</h3>
-                    <h4>By : {author}</h4>
+                    <h4 className="text-base font-bold">By : {author}</h4>
                     <div className="flex justify-between">
                         <p>{category}</p>
-                        <div>
+                        <div className="flex gap-2 items-center">
                             <p>{rating}</p>
+                            <FiStar />
                         </div>
                     </div>
                 </div>
