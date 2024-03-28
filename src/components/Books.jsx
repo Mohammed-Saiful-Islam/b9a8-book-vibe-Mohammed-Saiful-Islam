e7e64879d60725d65b96adcd8fd37da377addf89
Mohammed-Saiful-Islam <mosai21498@gmail.com>
2024-03-28 19:49:15 +0600

    section books some info edited

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,16 +1,25 @@
 
-const Books = ({book}) => {
-    const {bookName,author,image,review,totalPages,rating,category,tags,publisher,yearOfPublishing}=book
+const Books = ({ book }) => {
+    /* 
+    {bookName,author,image,review,totalPages,rating,category,tags,publisher,yearOfPublishing}
+    */
+    const { bookName, author, image, category, tags, rating } = book
     return (
-        <div>
-              <a rel="noopener noreferrer" href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-50">
-                            <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src={image} />
-                            <div className="p-6 space-y-2">
-                                <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">In usu laoreet repudiare legendos</h3>
-                                <span className="text-xs dark:text-gray-600">January 26, 2021</span>
-                                <p>Mei ex aliquid eleifend forensibus, quo ad dicta apeirian neglegentur, ex has tantas percipit perfecto. At per tempor albucius perfecto, ei probatus consulatu patrioque mea, ei vocent delicata indoctum pri.</p>
-                            </div>
-                        </a>
+        <div className="workSansFont">
+            <a rel="noopener noreferrer" href="#" className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-50">
+                <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src={image} />
+                <div className="p-6 space-y-2">
+                    <p>{tags}</p>
+                    <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">{bookName}</h3>
+                    <h4>By : {author}</h4>
+                    <div className="flex justify-between">
+                        <p>{category}</p>
+                        <div>
+                            <p>{rating}</p>
+                        </div>
+                    </div>
+                </div>
+            </a>
         </div>
     );
 };
